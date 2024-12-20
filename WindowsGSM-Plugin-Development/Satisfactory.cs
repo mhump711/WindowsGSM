3c2477c115bada2a9f00fc56e19e1ab4cf1350e5
mhump711 <mike.humpage@gmail.com>
2024-12-20 09:54:19 -0500

    First changes

---
+++ WindowsGSM-Plugin-Development/Satisfactory.cs
@@ -0,0 +1,16 @@
+using WindowsGSM.Functions;
+using WindowsGSM.GameServer.Engine;
+using WindowsGSM.GameServer.Query;
+using Newtonsoft.Json.Linq;
+
+namespace WindowsGSM.Plugins
+
+public Plugin Plugin = new Plugin
+{
+    name = "WindowsGSM.Satisfactory",
+    author = "Mike Humpage",
+    description = "🧩 WindowsGSM plugin for supporting Satisfactory Dedicated Server",
+    version = "1.0",
+    url = "https://github.com/XXXXXXXX/XXXXXXXX",
+    color = "#ffffff"
+};
